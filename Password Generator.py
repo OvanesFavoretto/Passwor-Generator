6bc254a2860c052e013cdbd49d3d3bd45f982849
Ovanes Favoretto <122630271+OvanesFavoretto@users.noreply.github.com>
2023-01-16 16:34:56 -0300

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,19 @@
+import random
+
+print('Welcome to your passwor generator100')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+number = input('Amount of password to generate: ')
+number = int(number)
+
+length = input('Input your password length: ')
+length = int(length)
+
+print('\nhere are your password:')
+
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print(passwords)
